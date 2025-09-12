74950ddf25e79c72180433b676cb6b5419ce3b0c
munanafisa <munanafisa538@gmail.com>
2025-09-12 11:23:24 +0700

    second publish

---
+++ src/modul_1/DataDiri/DataaDiri.java
@@ -0,0 +1,21 @@
+package modul_1.DataDiri;
+
+import java.time.LocalDate;
+
+public class DataaDiri {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+        String nama = "Muna nafisa";
+        String nim = "2024573010048";
+        LocalDate TglLahir= LocalDate.of(2006, 12,7);
+        String prodi= "Teknik Informatika";
+        String Jurusan = "Teknologi Informasi Dan Komputer";
+        int semester =3 ;
+        System.out.println("Nama :" + nama);
+        System.out.println("Nim :" + nim);
+        System.out.println("TglLahir :" + TglLahir);
+        System.out.println("prodi :" + prodi);
+        System.out.println("Jurusan :" + Jurusan);
+        System.out.println("semester :" + semester);
+    }
+}
